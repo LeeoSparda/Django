c1162e5f103808aa91c7eae1a2bb7631600280c6
LeeoSparda <leoanrdo_hitman@hotmail.com>
2024-02-01 16:18:35 -0300

    Versao 1.0.0

--- receita/views.py
+++ receita/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request,'recipes/home.html')
+    return render(request,'recipes/home.html', context={
+        'name':'Leonardo Borges',
+    })
 
 #HTTP REQUEST
 def contato(request):
-    return HttpResponse('CONTATO')
-
+    return render(request, 'recipes/contato.html')
 
 def sobre(request):
     return HttpResponse('SOBRE')
