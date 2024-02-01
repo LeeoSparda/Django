ddc1ed40e7ba7d141ec12b5adbbd2536ec99c8e8
LeeoSparda <leoanrdo_hitman@hotmail.com>
2024-02-01 15:00:07 -0300

    Versao

---
+++ receita/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('HOME')
+
+#HTTP REQUEST
+def contato(request):
+    return HttpResponse('CONTATO')
+
+
+def sobre(request):
+    return HttpResponse('SOBRE')
